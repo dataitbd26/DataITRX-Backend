0489d77b83729a95f9b0e0bd1c0f42be8b0f8716
Salauddin Khan Apon <salauddinkhanapon.77@gmail.com>
2026-03-11 14:46:32 +0600

    update backend

--- app/modules/Prescription/Prescription.model.js
+++ app/modules/Prescription/Prescription.model.js
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose';
 
-// 1. Medicine Sub-schema (Reused from Templates)
+
 const medicineSchema = new mongoose.Schema({
     name: { type: String, required: true },
     dosage: { type: String, default: '' },
